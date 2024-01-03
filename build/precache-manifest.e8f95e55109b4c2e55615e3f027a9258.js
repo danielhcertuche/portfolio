a97f4d4d9c68e462c4fa962e92f02c31f4c31f90
danhcertuche <danielcert3@gmail.com>
2024-01-02 23:27:20 -0500

    Sync

---
+++ build/precache-manifest.e8f95e55109b4c2e55615e3f027a9258.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df937107a181288cdea18ab344762cb4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "83e8288662cbf4349f26",
+    "url": "/portfolio/static/css/main.d3ea7e15.chunk.css"
+  },
+  {
+    "revision": "e5b1679f92af044e67b5",
+    "url": "/portfolio/static/js/2.aa4ae2e1.chunk.js"
+  },
+  {
+    "revision": "31ab0097190a9d99b331909bb0d9d9b1",
+    "url": "/portfolio/static/js/2.aa4ae2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "912394e270913998500c",
+    "url": "/portfolio/static/js/3.815aef19.chunk.js"
+  },
+  {
+    "revision": "83e8288662cbf4349f26",
+    "url": "/portfolio/static/js/main.70d04452.chunk.js"
+  },
+  {
+    "revision": "2d697fbeb7f3bf7d7cec",
+    "url": "/portfolio/static/js/runtime-main.4d504475.js"
+  },
+  {
+    "revision": "5dec418ab38d0c660c818fe342728b62",
+    "url": "/portfolio/static/media/DanielProfile.5dec418a.jpg"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/portfolio/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/portfolio/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/portfolio/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/portfolio/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/portfolio/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/portfolio/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/portfolio/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "da9246e52e92731103975802063b1aa1",
+    "url": "/portfolio/static/media/bg.da9246e5.jpg"
+  }
+]);
